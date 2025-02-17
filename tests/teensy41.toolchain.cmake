d581a030e05ca99c6cd17658db575a30d91036d0
newdigate <nicnewdigate@gmail.com>
2025-02-17 14:06:09 +0000

    download teensy cores

--- tests/teensy41.toolchain.cmake
+++ tests/teensy41.toolchain.cmake
@@ -1,11 +1,24 @@
 set(TEENSY_VERSION 41 CACHE STRING "Set to the Teensy version corresponding to your board (30 or 31 allowed)" FORCE)
 set(CPU_CORE_SPEED 600000000 CACHE STRING "Set to 24000000, 48000000, 72000000 or 96000000 to set CPU core speed" FORCE) # Derived variables
-#set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs" CACHE INTERNAL "")
+set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs" CACHE INTERNAL "")
+
 #teensy compiler options
 set(COMPILERPATH "/opt/gcc-arm-none-eabi-9-2019-q4-major/bin/")
 #set(COMPILERPATH "/Applications/ARM/bin/")
-set(DEPSPATH "/home/runner/work/teensy-cmake-macros/teensy-cmake-macros/deps")
-#set(DEPSPATH "/Users/nicholasnewdigate/Development/github/newdigate/temp_dep")
-set(COREPATH "${DEPSPATH}/cores/teensy4/")
 
-find_package(teensy_cmake_macros)  # use installed teensy_cmake_macros cmake package
+include(FetchContent)
+FetchContent_Declare(teensy_cmake_macros
+        GIT_REPOSITORY https://github.com/newdigate/teensy-cmake-macros
+        GIT_TAG        noinstall
+)
+FetchContent_MakeAvailable(teensy_cmake_macros)
+message(INFO ${teensy_cmake_macros_SOURCE_DIR})
+include(${teensy_cmake_macros_SOURCE_DIR}/CMakeLists.arm.include.txt)
+
+#set(BUILD_FOR_TEENSY ON)
+#set(CMAKE_SYSTEM_NAME Generic)
+#set(CMAKE_SYSTEM_PROCESSOR arm)
+#set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
+#set(CMAKE_C_COMPILER ${COMPILERPATH}arm-none-eabi-gcc)
+#set(CMAKE_CXX_COMPILER ${COMPILERPATH}arm-none-eabi-g++)
+#set(CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_C_COMPILER} <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
